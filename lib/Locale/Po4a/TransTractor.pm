ff9ffd75fd0c30f68399f841c9924456a81f524a
Martin Quinson <mquinson@debian.org>
2004-05-26 17:51:35 +0000

    Version update

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -10,7 +10,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.16";
+$VERSION="0.16.1";
 @ISA = ();
 @EXPORT = qw(process translate 
 	     read write readpo writepo);
