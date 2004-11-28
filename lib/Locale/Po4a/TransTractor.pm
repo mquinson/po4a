10aae3429dc9c5c91b4a1847db991a975c496af5
Denis Barbier <barbier@debian.org>
2004-11-28 21:30:43 +0000

    Bump $VERSION to 0.20

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -10,7 +10,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.19";
+$VERSION="0.20";
 @ISA = ();
 @EXPORT = qw(process translate 
 	     read write readpo writepo
