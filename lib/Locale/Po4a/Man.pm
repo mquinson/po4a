3a78940a044faef9c587fe6f4c505acf1c2481e0
Nicolas FRANÇOIS <nicolas.francois@centraliens.net>
2005-09-03 18:22:40 +0000

    Do not split the ta request's arguments because the number of spaces can be important.

--- lib/Locale/Po4a/Man.pm
+++ lib/Locale/Po4a/Man.pm
@@ -1004,7 +1004,13 @@ sub parse{
 	    my @args=();
 	    $line =~ s/\\ /$nbs/g; # This is probably not needed
 	    push @args,$arg1;
-	    push @args, splitargs($ref,$arguments);
+	    if ($macro =~ /^(?:ta|TP)$/) {
+		# The number of spaces may be critical for the 'ta' macro,
+		# and there is no need to split the arguments.
+		push @args, $arguments;
+	    } else {
+		push @args, splitargs($ref,$arguments)
+	    }
 
 
 	    if (length($paragraph)) {
