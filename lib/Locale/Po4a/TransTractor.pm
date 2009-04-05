4d7b2ee733b280a11f645dc4990bbd2963716b0d
Nicolas FRANÇOIS <nicolas.francois@centraliens.net>
2009-04-05 12:01:19 +0000

    Prepare the next release 0.36.1.

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.36";
+$VERSION="0.36.1";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate 
              read write readpo writepo
