b357d39a0d7cc2ef08f43b3169a295d6be78e93a
Nicolas FRANÇOIS <nicolas.francois@centraliens.net>
2009-11-07 12:59:59 +0000

    * NEWS, lib/Locale/Po4a/TransTractor.pm: Prepare next release 	* po/bin/po4a.pot, po/pod/po4a-pod.pot: Regenerated. 	* po/bin/*.po, po/pod/*.po: Synchronized with the POTs.

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.36.5";
+$VERSION="0.36.6";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate 
              read write readpo writepo
