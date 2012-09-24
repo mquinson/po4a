f66f67fc7b86713c58711a385f37bbc0189c7f2c
Denis Barbier <barbier@debian.org>
2012-09-24 22:06:33 +0000

    Bump version to 0.43 to prepare the next release

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.42";
+$VERSION="0.43";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate 
              read write readpo writepo
