ae37afb66ea9a12ab9b9d0b7218dbfbb6313058e
Martin Quinson <mquinson@debian.org>
2015-05-20 22:14:39 +0000

    bump the version number to 0.46

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.45";
+$VERSION="0.46";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate
              read write readpo writepo
