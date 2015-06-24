372c9a481910c4af1a7c4aa63af342dbbb6ea86e
Martin Quinson <mquinson@debian.org>
2015-06-24 22:13:27 +0000

    version 0.47 is ongoing

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.46";
+$VERSION="0.47";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate
              read write readpo writepo
