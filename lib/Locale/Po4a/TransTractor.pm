6e40af1f6fd021f15730f8da984c2042d100b697
Martin Quinson <martin.quinson@loria.fr>
2017-03-25 17:27:27 +0100

    Update French, thanks Alban Vidal (Closes: #856188)

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.48";
+$VERSION="0.49";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate
              read write readpo writepo
