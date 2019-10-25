ac30d2447f61d9ed99acb9721585dee2fe543b8c
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-10-25 12:32:55 +0200

    Release v0.57

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION="0.57-alpha";
+$VERSION="0.57";
 @ISA = qw(DynaLoader);
 @EXPORT = qw(new process translate
              read write readpo writepo
