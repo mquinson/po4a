e3875d4caf22f26cecaac3d1b9946c7ca1c0263b
Martin Quinson <martin.quinson@ens-rennes.fr>
2020-05-24 23:06:31 +0200

    Releasing v0.59.1

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = "0.60-alpha";
+$VERSION = "0.59.1";
 @ISA     = qw(DynaLoader);
 @EXPORT  = qw(new process translate
   read write readpo writepo
