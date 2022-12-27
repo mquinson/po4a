f1fe2f6461325a81f54d298f24324bbd6c7edf2d
Martin Quinson <martin.quinson@ens-rennes.fr>
2022-12-27 16:23:43 +0100

    mark another string for translation + tidyall this file

--- lib/Locale/Po4a/Text.pm
+++ lib/Locale/Po4a/Text.pm
@@ -767,9 +767,10 @@ sub parse_markdown {
         my $fencechar           = $3;
         my $fence_space_between = $4;
         my $info_string         = $5;
-#        print STDERR "----------------\n";
-#        print STDERR "line: $line\n";
-#        print STDERR "fence: '$fence'; fencechar: '$fencechar'; info: '$info_string'\n";
+
+        #        print STDERR "----------------\n";
+        #        print STDERR "line: $line\n";
+        #        print STDERR "fence: '$fence'; fencechar: '$fencechar'; info: '$info_string'\n";
 
         # fenced div block (fenced with ::: where code blocks are fenced with ` or ~)
         # https://pandoc.org/MANUAL.html#divs-and-spans
@@ -782,20 +783,23 @@ sub parse_markdown {
         $paragraph = "";
 
         my $lvl = 1;
-        while ( $lvl > 0) {
+        while ( $lvl > 0 ) {
             my ( $nextline, $nextref ) = $self->shiftline();
-            # TODO: Uncomment this once the string freeze is over
-            # die wrap_mod("po4a::text",
-            #             dgettext("po4a",
-            #                      "Malformed fenced div block: Block starting at %s not closed before the end of the file."), $ref)
-            #    unless (defined($nextline));
-#            print STDERR "within $lvl: $nextline";
-            if ($nextline =~ /^\s*:::+\s*$/ ) {
+            die wrap_mod(
+                "po4a::text",
+                dgettext(
+                    "po4a", "Malformed fenced div block: Block starting at %s not closed before the end of the file."
+                ),
+                $ref
+            ) unless ( defined($nextline) );
+
+            #            print STDERR "within $lvl: $nextline";
+            if ( $nextline =~ /^\s*:::+\s*$/ ) {
                 $lvl--;
-            } elsif ($nextline =~ /^([ ]{0,3})(([:])\3{2,})(\s*)([^`]*)\s*$/ ) {
+            } elsif ( $nextline =~ /^([ ]{0,3})(([:])\3{2,})(\s*)([^`]*)\s*$/ ) {
                 $lvl++;
             }
-            if ($lvl > 0) {
+            if ( $lvl > 0 ) {
                 $paragraph .= $nextline;
             } else {
                 do_paragraph( $self, $paragraph, $wrapped_mode, $type );
@@ -804,7 +808,8 @@ sub parse_markdown {
         }
         $paragraph        = "";
         $end_of_paragraph = 1;
-#        print STDERR "Out now ------------\n";
+
+        #        print STDERR "Out now ------------\n";
     } elsif (
         $line =~ /^\s*\[\[\!\S+\s*$/       # macro begin
         or $line =~ /^\s*"""\s*\]\]\s*$/
