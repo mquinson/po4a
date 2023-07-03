7cf7b79ee28de7e58378b225570d49fd114ff75e
Jan Palus <jpalus@fastmail.com>
2023-07-03 14:56:04 +0200

    SGML: replace invalid byte (0xa0) in comment with space (0x20)
    Fixes #428

--- lib/Locale/Po4a/Sgml.pm
+++ lib/Locale/Po4a/Sgml.pm
@@ -773,7 +773,7 @@ sub parse_file {
             while ( $origfile =~ /^(.*?)&$key(;.*$|[^-_:.A-Za-z0-9].*$|$)/s ) {
 
                 # Since we will include a new file, we
-                # must do a new round of substitutions.
+                # must do a new round of substitutions.
                 $dosubstitution = 1;
                 my ( $begin, $end ) = ( $1, $2 );
                 $end = "" unless ( defined $end );
