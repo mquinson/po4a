191b41f56400e4d1d38b0de2fbadf810b5added7
Martin Quinson <martin.quinson@ens-rennes.fr>
2024-06-17 22:36:39 +0200

    Start the 0.74 release cycle

--- lib/Locale/Po4a/TransTractor.pm
+++ lib/Locale/Po4a/TransTractor.pm
@@ -11,7 +11,7 @@ use warnings;
 
 use subs qw(makespace);
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = "0.73";
+$VERSION = "0.74-alpha";
 @ISA     = qw(DynaLoader);
 @EXPORT  = qw(new process translate
   read write readpo writepo
