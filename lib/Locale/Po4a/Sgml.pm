7ecdfe69f27dd1ebefec0f2b4fb1587facc0b1be
gemmaro <gemmaro.dev@gmail.com>
2024-09-03 12:13:24 +0900

    Fix typo in Sgml module
    * lib/Locale/Po4a/Sgml.pm: Fix a typo in the Sgml module.
    Reported by @yurchor at Weblate[1][2][3].
    [1] https://hosted.weblate.org/translate/po4a/po4a-doc/en/?q=has%3Acomment&offset=5
    [2] https://hosted.weblate.org/translate/po4a/po4a-doc/en/?q=has%3Acomment&offset=6
    [3] https://hosted.weblate.org/translate/po4a/po4a-doc/en/?q=has%3Acomment&offset=7

--- lib/Locale/Po4a/Sgml.pm
+++ lib/Locale/Po4a/Sgml.pm
@@ -75,7 +75,7 @@ containing other tags, some of them being of category B<translate>.
 =item B<indent>
 
 Space-separated list of tags which increase the indentation level. This will
-affect the identation in the resulting document.
+affect the indentation in the resulting document.
 
 =item B<verbatim>
 
@@ -92,7 +92,7 @@ Tags not needing to be closed.
 Tags ignored and considered as plain char data by po4a. That is to say that
 they can be part of an msgid. For example, E<lt>bE<gt> is a good candidate
 for this category since putting it in the B<translate> section would create a
-msgids with only its content, (and it's ususally not a whole sentence), which
+msgids with only its content, (and it's usually not a whole sentence), which
 is bad.
 
 =item B<attributes>
@@ -110,7 +110,7 @@ an C<< <aaa> >> or a C<< <bbb> >> tag.
 =item B<qualify>
 
 A space-separated list of attributes for which the translation must be
-qualified by the attribute name, i.e. the text extracted for the transalation
+qualified by the attribute name, i.e. the text extracted for the translation
 will include both the attributes name and it's value. e.g. for a tag like
 C<< <aaa lang_en="foo"> >> translators will be presented with the string
 C<lang_en="foo">. Note that this also automatically adds the given attribute
