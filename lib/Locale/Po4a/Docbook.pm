70bd72b889b7c54be8e59bf320f9340d774bd158
gemmaro <gemmaro.dev@gmail.com>
2024-10-13 13:50:05 +0900

    Use parent pragma in Docbook module

--- lib/Locale/Po4a/Docbook.pm
+++ lib/Locale/Po4a/Docbook.pm
@@ -99,11 +99,9 @@ use 5.16.0;
 use strict;
 use warnings;
 
-use Locale::Po4a::Common;
-use Locale::Po4a::Xml;
+use parent qw(Locale::Po4a::Xml);
 
-use vars qw(@ISA);
-@ISA = qw(Locale::Po4a::Xml);
+use Locale::Po4a::Common;
 
 sub initialize {
     my $self    = shift;
