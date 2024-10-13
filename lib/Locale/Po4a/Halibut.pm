3fb926670ab509fd26419269decade0ba8c4884f
gemmaro <gemmaro.dev@gmail.com>
2024-10-13 13:39:17 +0900

    Use parent pragma in Halibut module

--- lib/Locale/Po4a/Halibut.pm
+++ lib/Locale/Po4a/Halibut.pm
@@ -81,14 +81,14 @@ use 5.16.0;
 use strict;
 use warnings;
 
+use parent qw(Locale::Po4a::TeX);
+
 require Exporter;
-use vars qw($VERSION @ISA @EXPORT);
+use vars qw($VERSION @EXPORT);
 $VERSION = $Locale::Po4a::TeX::VERSION;
-@ISA     = qw(Locale::Po4a::TeX);
 @EXPORT  = qw();
 
 use Locale::Po4a::Common;
-use Locale::Po4a::TeX;
 use subs qw(&parse_definition_file
   &register_generic_command &is_closed &translate_buffer
   &register_verbatim_environment
