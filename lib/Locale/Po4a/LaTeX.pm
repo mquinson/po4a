b44b4cad53f02726844044d3f202e37a5d463e8a
gemmaro <gemmaro.dev@gmail.com>
2024-10-14 13:40:31 +0900

    Removed the empty export-list from the LaTeX module
    * lib/Locale/Po4a/LaTeX.pm: Removed the empty export-list from the
    LaTeX module.  The @EXPORT list was empty.

--- lib/Locale/Po4a/LaTeX.pm
+++ lib/Locale/Po4a/LaTeX.pm
@@ -69,11 +69,8 @@ use strict;
 use warnings;
 
 use parent qw(Locale::Po4a::TeX);
-
-require Exporter;
-use vars qw($VERSION @EXPORT);
+use vars qw($VERSION);
 $VERSION = $Locale::Po4a::TeX::VERSION;
-@EXPORT  = qw();
 
 use subs qw(&generic_command
   &parse_definition_file
