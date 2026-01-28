bf00c794f81a26d8c339cbeb37d683f112322372
gemmaro <gemmaro.dev@gmail.com>
2026-01-28 13:28:12 +0900

    Tidy Po4aBuilder module.

--- Po4aBuilder.pm
+++ Po4aBuilder.pm
@@ -11,10 +11,10 @@ use warnings;
 use parent qw(Module::Build);
 
 use File::Basename qw(fileparse);
-use File::Path qw(mkpath rmtree);
-use File::Spec ();
-use File::Copy qw(copy);
-use File::stat qw(lstat stat);
+use File::Path     qw(mkpath rmtree);
+use File::Spec     ();
+use File::Copy     qw(copy);
+use File::stat     qw(lstat stat);
 
 use IPC::Open3 qw(open3);
 
@@ -22,15 +22,15 @@ sub ACTION_build {
     my $self = shift;
     $self->depends_on('code');
     $self->depends_on('docs');
-    $self->depends_on('distmeta'); # regenerate META.yml
-    $self->depends_on('man') unless ($^O eq 'MSWin32');
-    $self->depends_on('postats') unless ($^O eq 'MSWin32');
+    $self->depends_on('distmeta');    # regenerate META.yml
+    $self->depends_on('man')     unless ( $^O eq 'MSWin32' );
+    $self->depends_on('postats') unless ( $^O eq 'MSWin32' );
 }
 
 sub make_files_writable {
-    my $self = shift;
-    my $dir = shift;
-    my $files = $self->rscan_dir($dir, sub {-f});
+    my $self  = shift;
+    my $dir   = shift;
+    my $files = $self->rscan_dir( $dir, sub { -f } );
     foreach my $file (@$files) {
         my $current_mode = stat($file)->mode;
         chmod $current_mode | oct(200), $file;
@@ -38,8 +38,7 @@ sub make_files_writable {
 }
 
 sub perl_scripts {
-    return ('po4a-gettextize', 'po4a-updatepo', 'po4a-translate',
-            'po4a-normalize', 'po4a', 'msguntypot');
+    return ( 'po4a-gettextize', 'po4a-updatepo', 'po4a-translate', 'po4a-normalize', 'po4a', 'msguntypot' );
 }
 
 # Update po/bin/*.po files
@@ -49,30 +48,32 @@ sub ACTION_binpo {
     $self->depends_on('code');
     $self->make_files_writable("po/bin");
 
-    my @all_files = sort((perl_scripts(), @{$self->rscan_dir('lib',qr{\.pm$})}));
-    unless ($self->up_to_date(\@all_files, "po/bin/po4a.pot")) {
+    my @all_files = sort( ( perl_scripts(), @{ $self->rscan_dir( 'lib', qr{\.pm$} ) } ) );
+    unless ( $self->up_to_date( \@all_files, "po/bin/po4a.pot" ) ) {
         print "XX Update po/bin/po4a.pot\n";
         chdir "po/bin";
-        my @sources = map { "../../".$_ } @all_files;
-        my @cmd = ("xgettext");
+        my @sources = map { "../../" . $_ } @all_files;
+        my @cmd     = ("xgettext");
         push @cmd, "--from-code=utf-8";
         push @cmd, "-L", "Perl";
         push @cmd, "--add-comments";
         push @cmd, "--msgid-bugs-address", "po4a\@packages.debian.org";
-        push @cmd, "--package-name", "po4a";
-        push @cmd, "--package-version", $self->dist_version();
+        push @cmd, "--package-name",       "po4a";
+        push @cmd, "--package-version",    $self->dist_version();
         push @cmd, @sources;
         push @cmd, "-o", "po4a.pot.new";
         system(@cmd) && die;
-	
+
         chdir "../..";
-	
-        if ( -e "po/bin/po4a.pot") {
-            my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
+
+        if ( -e "po/bin/po4a.pot" ) {
+            my $diff =
+              qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' po/bin/po4a.pot po/bin/po4a.pot.new);
             if ( $diff eq "" ) {
                 unlink "po/bin/po4a.pot.new" or die;
+
                 # touch it
-                my ($atime, $mtime) = (time,time);
+                my ( $atime, $mtime ) = ( time, time );
                 utime $atime, $mtime, "po/bin/po4a.pot";
             } else {
                 rename "po/bin/po4a.pot.new", "po/bin/po4a.pot" or die;
@@ -84,32 +85,35 @@ sub ACTION_binpo {
         print "XX po/bin/po4a.pot uptodate.\n";
     }
 
-    foreach (@{$self->rscan_dir('po/bin',qr{\.po$})}) {
-        my $lang = fileparse($_, qw{.po});
-      # Only update german languages. The others updated by the weblate robot directly
-      if ($lang eq 'de') {
-        unless ($self->up_to_date("po/bin/po4a.pot", $_)) {
-            print "XX Sync $_: ";
-            system("msgmerge", "--previous", $_, "po/bin/po4a.pot", "-o", "$_.new") && die;
-            # Typically all that changes was a date. I'd
-            # prefer not to commit such changes, so detect
-            # and ignore them.
-            my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
-            if ($diff eq "") {
-                unlink "$_.new" or die;
-                # touch it
-                my ($atime, $mtime) = (time,time);
-                utime $atime, $mtime, $_;
+    foreach ( @{ $self->rscan_dir( 'po/bin', qr{\.po$} ) } ) {
+        my $lang = fileparse( $_, qw{.po} );
+
+        # Only update german languages. The others updated by the weblate robot directly
+        if ( $lang eq 'de' ) {
+            unless ( $self->up_to_date( "po/bin/po4a.pot", $_ ) ) {
+                print "XX Sync $_: ";
+                system( "msgmerge", "--previous", $_, "po/bin/po4a.pot", "-o", "$_.new" ) && die;
+
+                # Typically all that changes was a date. I'd
+                # prefer not to commit such changes, so detect
+                # and ignore them.
+                my $diff = qx(diff -q -I'#:' -I'POT-Creation-Date:' -I'PO-Revision-Date:' $_ $_.new);
+                if ( $diff eq "" ) {
+                    unlink "$_.new" or die;
+
+                    # touch it
+                    my ( $atime, $mtime ) = ( time, time );
+                    utime $atime, $mtime, $_;
+                } else {
+                    rename "$_.new", $_ or die;
+                }
             } else {
-                rename "$_.new", $_ or die;
+                print "XX $_ uptodate.\n";
             }
-        } else {
-            print "XX $_ uptodate.\n";
         }
-      }
-        unless ($self->up_to_date($_,"blib/po/$lang/LC_MESSAGES/po4a.mo")) {
+        unless ( $self->up_to_date( $_, "blib/po/$lang/LC_MESSAGES/po4a.mo" ) ) {
             mkpath( File::Spec->catdir( 'blib', 'po', $lang, "LC_MESSAGES" ), 0, oct(755) );
-            system("msgfmt", "-o", "blib/po/$lang/LC_MESSAGES/po4a.mo", $_) && die;
+            system( "msgfmt", "-o", "blib/po/$lang/LC_MESSAGES/po4a.mo", $_ ) && die;
         }
     }
 }
@@ -118,19 +122,20 @@ sub ACTION_install {
     my $self = shift;
 
     require ExtUtils::Install;
-#    print ("KEYS\n");
-#    foreach my $k ($self->install_types()) {
-#	print ("$k -> ".$self->install_destination($k)."\n");
-#    }
+
+    #    print ("KEYS\n");
+    #    foreach my $k ($self->install_types()) {
+    #	print ("$k -> ".$self->install_destination($k)."\n");
+    #    }
     my $mandir = $self->install_destination('libdoc');
     $mandir =~ s,/man3$,,;
-    $self->install_path(man => $mandir);
+    $self->install_path( man => $mandir );
 
     my $localedir = $self->install_destination('libdoc');
     $localedir =~ s,/man/man3$,/locale,;
-    $self->install_path(po => $localedir);
+    $self->install_path( po => $localedir );
 
-    ExtUtils::Install::install($self->install_map, !$self->quiet, 0, $self->{args}{uninst}||0);
+    ExtUtils::Install::install( $self->install_map, !$self->quiet, 0, $self->{args}{uninst} || 0 );
 }
 
 sub ACTION_dist {
@@ -145,7 +150,8 @@ sub ACTION_dist {
     my $dist_dir = $self->dist_dir;
     $self->make_files_writable($dist_dir);
 
-    if ( -e "$dist_dir.tar.gz") {
+    if ( -e "$dist_dir.tar.gz" ) {
+
         # Delete the distfile if it already exists
         unlink "$dist_dir.tar.gz" or die;
     }
@@ -159,16 +165,16 @@ sub ACTION_docpo {
     $self->depends_on('code');
     $self->make_files_writable("po/pod");
 
-    my @cmd = ("perl", "-Ilib", "po4a"); # Use this version of po4a
+    my @cmd = ( "perl", "-Ilib", "po4a" );    # Use this version of po4a
     push @cmd, "--previous";
     push @cmd, "--no-translations";
     push @cmd, "--msgid-bugs-address", "devel\@lists.po4a.org";
-    push @cmd, "--package-name", "po4a";
-    push @cmd, "--package-version", $self->dist_version();
-    push @cmd, split(" ", $ENV{PO4AFLAGS}) if defined($ENV{PO4AFLAGS});
+    push @cmd, "--package-name",       "po4a";
+    push @cmd, "--package-version",    $self->dist_version();
+    push @cmd, split( " ", $ENV{PO4AFLAGS} ) if defined( $ENV{PO4AFLAGS} );
     push @cmd, "po/pod.cfg";
     system(@cmd)
-        and die;
+      and die;
 }
 
 sub ACTION_man {
@@ -186,8 +192,8 @@ sub ACTION_man {
     my $manpath = File::Spec->catdir( 'blib', 'man' );
     File::Path::rmtree( $manpath, 0, 1 );
 
-    my @cmd = ("perl", "-Ilib", "po4a");    # Use this version of po4a
-    push @cmd, split(" ", $ENV{PO4AFLAGS}) if defined( $ENV{PO4AFLAGS} );
+    my @cmd = ( "perl", "-Ilib", "po4a" );    # Use this version of po4a
+    push @cmd, split( " ", $ENV{PO4AFLAGS} ) if defined( $ENV{PO4AFLAGS} );
     push @cmd, "--previous", "po/pod.cfg";
     system(@cmd) and die;
 
@@ -243,7 +249,7 @@ sub ACTION_man {
         }
         $parser->parse_from_file( $file, $out );
 
-        system("gzip", "-9", "-n", "-f", $out) and die;
+        system( "gzip", "-9", "-n", "-f", $out ) and die;
         unlink "$file" or die;
     }
 
@@ -270,12 +276,13 @@ sub ACTION_man {
                 my ( $outdir, $section, $outfile ) = ( $1, $2, $3 );
                 if ($local_docbook_xsl) {
                     print "Convert $outdir/$outfile.$section (local docbook.xsl file). ";
-                    system("xsltproc", "-o", "$outdir/$outfile.$section", "--nonet", $local_docbook_xsl, $file) and die;
+                    system( "xsltproc", "-o", "$outdir/$outfile.$section", "--nonet", $local_docbook_xsl, $file )
+                      and die;
                 } else {    # Not found locally, use the XSL file online
                     print "Convert $outdir/$outfile.$section (online docbook.xsl file). ";
-                    system("xsltproc", "-o", "$outdir/$outfile.$section", "--nonet", $docbook_xsl_url, $file) and die;
+                    system( "xsltproc", "-o", "$outdir/$outfile.$section", "--nonet", $docbook_xsl_url, $file ) and die;
                 }
-                system("gzip", "-9", "-n", "-f", "$outdir/$outfile.$section") and die;
+                system( "gzip", "-9", "-n", "-f", "$outdir/$outfile.$section" ) and die;
             }
             unlink "$file" or die;
         }
@@ -287,70 +294,126 @@ sub ACTION_postats {
     $self->depends_on('binpo');
     $self->depends_on('docpo');
     print("-------------\n");
-    $self->postats( File::Spec->catdir("po", "bin"));
+    $self->postats( File::Spec->catdir( "po", "bin" ) );
     print("-------------\n");
-    $self->postats( File::Spec->catdir("po", "pod"));
+    $self->postats( File::Spec->catdir( "po", "pod" ) );
     print("-------------\n");
-    $self->postats( File::Spec->catdir("..", "po4a-website", "po")) if -d File::Spec->catdir("..", "po4a-website", "po");
+    $self->postats( File::Spec->catdir( "..", "po4a-website", "po" ) )
+      if -d File::Spec->catdir( "..", "po4a-website", "po" );
 }
 
 sub postats {
-    my ($self,$dir) = (shift,shift);
-    my $potfiles = $self->rscan_dir($dir,qr{\.pot$});
+    my ( $self, $dir ) = ( shift, shift );
+    my $potfiles = $self->rscan_dir( $dir, qr{\.pot$} );
     die "No POT file found in $dir" unless scalar $potfiles;
     my $potfile = pop @$potfiles;
     my $potsize = stat($potfile)->size;
     print "$dir (pot: $potsize)\n";
-    my @files = @{$self->rscan_dir($dir,qr{\.po$})};
-    my (@t100,@t95,@t90,@t80,@t70,@t50,@t33,@t20,@starting);
-    foreach my $file (sort @files) {
-        my $lang = fileparse($file, qw{.po});
+    my @files = @{ $self->rscan_dir( $dir, qr{\.po$} ) };
+    my ( @t100, @t95, @t90, @t80, @t70, @t50, @t33, @t20, @starting );
+
+    foreach my $file ( sort @files ) {
+        my $lang = fileparse( $file, qw{.po} );
         my $stat = `msgfmt -o /dev/null -c --statistics $file 2>&1`;
-	my ($trans, $fuzz, $untr) = (0,0,0);
-	if ($stat =~ /(\d+)\D+?(\d+)\D+?(\d+)/) {
-	  ($trans, $fuzz, $untr) = ($1,$2,$3);
-	} elsif ($stat =~ /(\d+)\D+?(\d+)/) {
-	  ($trans, $fuzz) = ($1,$2);
-	} elsif ($stat =~ /(\d+)/) {
-	  ($trans) = ($1);
-	} else {
-	  print "Unparsable content: $stat\n";
-	}
-        my $total = $trans+$fuzz+$untr;
-	my $ratio = $trans / $total * 100;
-#	print "ratio: $ratio| trans: $trans; fuzz: $fuzz; untr: $untr\n";
-	my $Ratio = int($ratio);
-        print "  $lang (".(int($ratio*100)/100)."%): $stat";
-	if ($ratio == 100) {
-	    push @t100, $lang;
-	} elsif ($ratio >= 95) {
-	    push @t95, "$lang ($Ratio%)";
-	} elsif ($ratio >= 90) {
-	    push @t90, "$lang ($Ratio%)";
-	} elsif ($ratio >= 80) {
-	    push @t80, "$lang ($Ratio%)";
-	} elsif ($ratio >= 70) {
-	    push @t70, "$lang ($Ratio%)";
-	} elsif ($ratio >= 50) {
-	    push @t50, "$lang ($Ratio%)";
-	} elsif ($ratio >= 33) {
-	    push @t33, "$lang ($Ratio%)";
-	} elsif ($ratio >= 20) {
-	    push @t20, "$lang ($Ratio%)";
-	} else {
-	    push @starting, "$lang ($Ratio%)";
-	}
+        my ( $trans, $fuzz, $untr ) = ( 0, 0, 0 );
+        if ( $stat =~ /(\d+)\D+?(\d+)\D+?(\d+)/ ) {
+            ( $trans, $fuzz, $untr ) = ( $1, $2, $3 );
+        } elsif ( $stat =~ /(\d+)\D+?(\d+)/ ) {
+            ( $trans, $fuzz ) = ( $1, $2 );
+        } elsif ( $stat =~ /(\d+)/ ) {
+            ($trans) = ($1);
+        } else {
+            print "Unparsable content: $stat\n";
+        }
+        my $total = $trans + $fuzz + $untr;
+        my $ratio = $trans / $total * 100;
+
+        #	print "ratio: $ratio| trans: $trans; fuzz: $fuzz; untr: $untr\n";
+        my $Ratio = int($ratio);
+        print "  $lang (" . ( int( $ratio * 100 ) / 100 ) . "%): $stat";
+        if ( $ratio == 100 ) {
+            push @t100, $lang;
+        } elsif ( $ratio >= 95 ) {
+            push @t95, "$lang ($Ratio%)";
+        } elsif ( $ratio >= 90 ) {
+            push @t90, "$lang ($Ratio%)";
+        } elsif ( $ratio >= 80 ) {
+            push @t80, "$lang ($Ratio%)";
+        } elsif ( $ratio >= 70 ) {
+            push @t70, "$lang ($Ratio%)";
+        } elsif ( $ratio >= 50 ) {
+            push @t50, "$lang ($Ratio%)";
+        } elsif ( $ratio >= 33 ) {
+            push @t33, "$lang ($Ratio%)";
+        } elsif ( $ratio >= 20 ) {
+            push @t20, "$lang ($Ratio%)";
+        } else {
+            push @starting, "$lang ($Ratio%)";
+        }
     }
     print "$dir (pot: $potsize)\n";
-    print " ".(scalar (@t100))." language".(scalar (@t100)==1?' ':'s')." = 100%: ". (join(", ", @t100)).".\n" if (scalar(@t100)>0);
-    print " ".(scalar (@t95))." language".(scalar (@t95)==1?' ':'s')." >= 95%: ". (join(", ", @t95)).".\n" if (scalar(@t95)>0);
-    print " ".(scalar (@t90))." language".(scalar (@t90)==1?' ':'s')." >= 90%: ". (join(", ", @t90)).".\n" if (scalar(@t90)>0);
-    print " ".(scalar (@t80))." language".(scalar (@t80)==1?' ':'s')." >= 80%: ". (join(", ", @t80)).".\n" if (scalar(@t80)>0);
-    print " ".(scalar (@t70))." language".(scalar (@t70)==1?' ':'s')." >= 70%: ". (join(", ", @t70)).".\n" if (scalar(@t70)>0);
-    print " ".(scalar (@t50))." language".(scalar (@t50)==1?' ':'s')." >= 50%: ". (join(", ", @t50)).".\n" if (scalar(@t50)>0);
-    print " ".(scalar (@t33))." language".(scalar (@t33)==1?' ':'s')." >= 33%: ". (join(", ", @t33)).".\n" if (scalar(@t33)>0);
-    print " ".(scalar (@t20))." language".(scalar (@t20)==1?' ':'s')." >= 20%: ". (join(", ", @t20)).".\n" if (scalar(@t20)>0);
-    print " ".(scalar (@starting))." starting language".(scalar (@starting)==1?' ':'s').": ". (join(", ", @starting)).".\n" if (scalar(@starting)>0);
+    print " "
+      . ( scalar(@t100) )
+      . " language"
+      . ( scalar(@t100) == 1 ? ' ' : 's' )
+      . " = 100%: "
+      . ( join( ", ", @t100 ) ) . ".\n"
+      if ( scalar(@t100) > 0 );
+    print " "
+      . ( scalar(@t95) )
+      . " language"
+      . ( scalar(@t95) == 1 ? ' ' : 's' )
+      . " >= 95%: "
+      . ( join( ", ", @t95 ) ) . ".\n"
+      if ( scalar(@t95) > 0 );
+    print " "
+      . ( scalar(@t90) )
+      . " language"
+      . ( scalar(@t90) == 1 ? ' ' : 's' )
+      . " >= 90%: "
+      . ( join( ", ", @t90 ) ) . ".\n"
+      if ( scalar(@t90) > 0 );
+    print " "
+      . ( scalar(@t80) )
+      . " language"
+      . ( scalar(@t80) == 1 ? ' ' : 's' )
+      . " >= 80%: "
+      . ( join( ", ", @t80 ) ) . ".\n"
+      if ( scalar(@t80) > 0 );
+    print " "
+      . ( scalar(@t70) )
+      . " language"
+      . ( scalar(@t70) == 1 ? ' ' : 's' )
+      . " >= 70%: "
+      . ( join( ", ", @t70 ) ) . ".\n"
+      if ( scalar(@t70) > 0 );
+    print " "
+      . ( scalar(@t50) )
+      . " language"
+      . ( scalar(@t50) == 1 ? ' ' : 's' )
+      . " >= 50%: "
+      . ( join( ", ", @t50 ) ) . ".\n"
+      if ( scalar(@t50) > 0 );
+    print " "
+      . ( scalar(@t33) )
+      . " language"
+      . ( scalar(@t33) == 1 ? ' ' : 's' )
+      . " >= 33%: "
+      . ( join( ", ", @t33 ) ) . ".\n"
+      if ( scalar(@t33) > 0 );
+    print " "
+      . ( scalar(@t20) )
+      . " language"
+      . ( scalar(@t20) == 1 ? ' ' : 's' )
+      . " >= 20%: "
+      . ( join( ", ", @t20 ) ) . ".\n"
+      if ( scalar(@t20) > 0 );
+    print " "
+      . ( scalar(@starting) )
+      . " starting language"
+      . ( scalar(@starting) == 1 ? ' ' : 's' ) . ": "
+      . ( join( ", ", @starting ) ) . ".\n"
+      if ( scalar(@starting) > 0 );
 }
 
 1;
